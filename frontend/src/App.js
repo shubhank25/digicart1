df53f9c539f956fccaef8736676f4fc903f19f9a
m-triple-m <triplem656@gmail.com>
2021-07-07 16:54:39 +0530

    ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Header } from './component/header.js';
+import Header from './components/header';
+import Login from './components/login';
+import Register from './components/register';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div>
       <Router>
-        <Route component={Header} />
+        <Header />
+        <Route component={Register} path={'/register'} />
+        <Route component={Login} path={'/login'} />
+        <Footer />
       </Router>
     </div>
   );
