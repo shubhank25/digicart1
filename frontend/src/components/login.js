2bad9e26a50779eb605056e7fe5785f2c3529da3
shubhank25 <shubhanksingh565@gmail.com>
2021-07-31 17:03:32 +0530

    commit

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,25 +1,56 @@
-import './login.css';
+import { useState } from 'react';
+import app_config from '../config';
 
 const Login = () => {
 
-    return (
-        <div className="my-card">
-            <h3 className="title">Login Here</h3>
-            <hr />
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-            <form>
+    const url = app_config.api_url;
+
+    const submitForm = () => {
+
+        console.log(email, password);
+
+        fetch(url + '/api/user/getbyemail/' + email)
+            .then(res => res.json())
+            .then((data) => {
+                console.log(data);
+
+                if (data) {
+                    if (data.password == password) {
+                        console.log('login success');
+                        Swal.fire({
+                            icon: ''
+                        })
+
+                    } else {
+                        console.log('password incorrect');
+                    }
+                } else {
+                    console.log('user not found');
+                }
+
+            })
+
+    }
+
+    return (
+        <div className="col-md-2 mx-auto mt-5">
+            <div className="my-card mt-5" style={{ height: '70vh' }}>
+                <h3 className="title">Login Here</h3>
+                <hr />
 
                 <label htmlFor="">Email</label>
-                <input className="form-control" autoFocus type="text" />  
+                <input className="form-control" autoFocus type="text" onChange={(e) => { setEmail(e.target.value) }} />
 
                 <label htmlFor="">Password</label>
-                <input className="form-control" type="password" />
+                <input className="form-control" type="password" onChange={(e) => { setPassword(e.target.value) }} />
 
-                <button className="btn btn-outline-success mt-5">Login</button>
-
-            </form>
+                <button className="btn btn-outline-success mt-5" onClick={submitForm}>Login</button>
 
 
+            </div>
         </div>
     )
 }
